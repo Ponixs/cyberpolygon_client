1f0ae5ac57fc2ae4a69b40db81ad34c1c83d6419
tend <rydenko.urii@mail.ru>
2023-04-23 18:14:32 +0300

    отрефакторил структуру папок

---
+++ src/services/ADMService/ADMMaterials.js
@@ -0,0 +1,13 @@
+export class ADMMaterials {
+  static async addMaterial(materialInfo) {
+    console.log()
+  }
+
+  static async changeMaterial(materialInfo) {
+    console.log()
+  }
+
+  static async delMaterial(materialId) {
+    console.log()
+  }
+}
---
+++ src/services/ADMService/ADMTasks.js
@@ -0,0 +1,13 @@
+export class ADMTasks {
+  static async addTask(taskInfo) {
+    console.log()
+  }
+
+  static async changeTask(taskInfo) {
+    console.log()
+  }
+
+  static async delTask(taskId) {
+    console.log()
+  }
+}
