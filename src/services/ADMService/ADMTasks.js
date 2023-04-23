c2c9a2b2358225a6baf372cc8efac151ed8abaa8
Ten-Do <107281193+Ten-Do@users.noreply.github.com>
2023-04-24 01:08:32 +0300

    Update ADMTasks.js
    добавил комментарии

--- src/services/ADMService/ADMTasks.js
+++ src/services/ADMService/ADMTasks.js
@@ -3,10 +3,15 @@ import api from "../../http/api"
 export class ADMTasks {
   static async addTask(taskInfo) {
       api.post('/tasks',taskInfo ).then(response => {
-        if(response.status === 200)
-          console.log(response)
+        console.log(response)
+        // сделать так чтобы отображалось сообщение об успешном добавлении
+        // это может быть попап (не рекомендуемо)   https://habr.com/ru/articles/417873/
+        // либо снекбар (было бы идеально)          https://html5css.ru/howto/howto_js_snackbar.php
       }).catch(err => {
         console.log(err)
+        // аналогично с тем что выше, но желательно сделать более заметно чем снекбар
+        // как - полет вашей фантазии
+        // когда сдлеаешь - консоль логи надо убрать. в проде они нам не нужны
       })
   }
 
