bee94ed899ac6d215ac060e2a66083231cc57afe
Kirill Shokurov <kirill.shourov@gmail.com>
2023-04-28 21:23:13 +0300

    добавил часть запросов для юзера

--- src/components/pages/ADM/users/Requests/Requests.jsx
+++ src/components/pages/ADM/users/Requests/Requests.jsx
@@ -1,8 +1,9 @@
 import { Request } from "../../uiComponents/Request";
 
+
 export const Requests = (props) => {
     let RequestElements = props.data
-        .map(d => <Request data = {d} /> );
+        .map(d => <Request data={d} />);
     return (
         <div className="request">
             <div className="request_tittle">Заявки</div>
--- src/services/ADMService/ADMUsers.js
+++ src/services/ADMService/ADMUsers.js
@@ -1,13 +1,48 @@
+import api from "../../http/api"
+import { PATHS } from "../../utils/urls"
+
 export class ADMUsers {
-  static async verifyUser(userId) {
-    console.log()
+  static async createUser(userInfo){
+    await api.post(`${PATHS.register}`, {
+      email: userInfo.email,
+      nickname: userInfo.nickname,
+      name: userInfo.name,
+      surname: userInfo.surname,
+      categories: userInfo.categories,
+      studentCard: userInfo.studentCard
+    }).then(res => console.log(res))
+
+  }
+
+  static async getUsers(){
+    let users
+    await api.get(`${PATHS.users}`).then((res) => {
+      users = res.data
+      console.log(res)
+    }).catch(err => console.error(err))
+    return users
+  }
+
+  static async verifyUser(userInfo) {
+    await api.post(`${PATHS.activation}/${userInfo.id}`,{
+      email: userInfo.email,
+      solved: true,
+      comment: userInfo.comment
+    }).then(res => console.log(res)).catch(err => console.error(err))
   }
 
   static async updateUser(userInfo) {
-    console.log()
+    await api.put(`${PATHS.user}`, {
+      nickname: userInfo.nickname,
+      password: userInfo.password,
+      delCategories: userInfo.delCategories,
+      addCategories: userInfo.addCategories
+    }).then(res=>console.log(res.data)).catch(err => console.error(err))
+  
   }
 
   static async delUser(userId) {
-    console.log()
+    await api.delete(`${PATHS.users}/${userId}`).then(res => console.log(res.data)).catch(err=>console.error(err))
+    
   }
 }
